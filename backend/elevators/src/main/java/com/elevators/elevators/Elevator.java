bfccec237a5df257988176c80bef576552584215
Aleksandra <91892932+aleksandragoryczka@users.noreply.github.com>
2023-05-13 09:57:36 +0200

    Threads added, many elevators service

--- backend/elevators/src/main/java/com/elevators/elevators/Elevator.java
+++ backend/elevators/src/main/java/com/elevators/elevators/Elevator.java
@@ -1,18 +1,118 @@
 package com.elevators.elevators;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Vector;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
-@AllArgsConstructor
 @Getter
 @Setter
-public class Elevator {
+public class Elevator implements Runnable {
     private int id;
     private int currentFloor;
     private List<Integer> destinationFloors;
     private boolean isMovingUp;
+    private boolean running;
+
+    public Elevator(int elevatorId, int initialFloor, List<Integer> destinationFloors, boolean isMovingUp) {
+        this.id = elevatorId;
+        this.currentFloor = initialFloor;
+        this.destinationFloors = destinationFloors;
+        this.running = false;
+        this.isMovingUp = isMovingUp;
+    }
+
+    @Override
+    public void run() {
+        running = true;
+        List<Integer> seekSequence = new ArrayList<Integer>();
+        // int nextFloor;
+
+        // System.out.println("current: " + elevator.getCurrentFloor() + "winda: " +
+        // elevator.getId());
+        while (running) {
+            if (destinationFloors.contains(currentFloor)) {
+                destinationFloors.remove(currentFloor);
+                // elevator.getDestinationFloors().remove(elevator.getCurrentFloor());
+                System.out.println("winda jest juz na requestowanym pietrze");
+                continue;
+            }
+            if (destinationFloors.isEmpty()) {
+                // System.out.println("Prosze daj pietro na ktore chcesz jechac");
+                continue;
+            }
+            System.out.println("podane piera: " + destinationFloors);
+
+            // jesli jest na drodze to jest ok -> co jesli dostanie inne zgloszenie - "z
+            // drugiej strony"
+            seekSequence = this.SCANAlgoritm(destinationFloors, currentFloor, isMovingUp);
+
+            while (!seekSequence.isEmpty()) {
+                int move = isMovingUp ? 1 : -1;
+                for (int i = currentFloor + move; i != seekSequence.get(0) + move; i += move) {
+                    currentFloor = i;
+                    System.out.println(id + " jest na: " + currentFloor);
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                seekSequence.remove(0);
+                destinationFloors = seekSequence;
+                // elevator.setDestinationFloors(seekSequence);
+            }
+        }
+        // TODO Auto-generated method stub
+        // throw new UnsupportedOperationException("Unimplemented method 'run'");
+    }
+
+    private List<Integer> SCANAlgoritm(List<Integer> destinationFloorsList, int currentFloor, boolean isMovingUp) {
+        Vector<Integer> up = new Vector<Integer>(), down = new Vector<Integer>();
+        int nowOnFloor = 0;
+        Vector<Integer> seekSequence = new Vector<Integer>();
+
+        for (int i = 0; i < destinationFloorsList.size(); i++) {
+            if (destinationFloorsList.get(i) < currentFloor) {
+                down.add(destinationFloorsList.get(i));
+            }
+            if (destinationFloorsList.get(i) > currentFloor) {
+                up.add(destinationFloorsList.get(i));
+            }
+        }
+
+        Collections.sort(down);
+        Collections.sort(up);
+
+        // napisac wybor czy ma najpierw zaczac poruszac sie w prawo czy w lewo
+        int run = 2;
+        while (run-- > 0) {
+            if (!isMovingUp) {
+                for (int i = down.size() - 1; i >= 0; i--) {
+                    nowOnFloor = down.get(i);
+
+                    // do pozniejszego wypisywania
+                    seekSequence.add(nowOnFloor);
+
+                    currentFloor = nowOnFloor;
+                }
+                isMovingUp = true;
+            } else {
+                for (int i = 0; i < up.size(); i++) {
+                    nowOnFloor = up.get(i);
+
+                    seekSequence.add(nowOnFloor);
+                    currentFloor = nowOnFloor;
+                }
+                isMovingUp = false;
+            }
+        }
+        System.out.println("seekSequence" + seekSequence);
+        return seekSequence;
+    }
 
 }
--- backend/elevators/src/main/java/com/elevators/elevators/ElevatorServiceImpl.java
+++ backend/elevators/src/main/java/com/elevators/elevators/ElevatorServiceImpl.java
@@ -23,13 +23,16 @@ enum isMovingUp {
 public class ElevatorServiceImpl implements ElevatorService {
     private List<Elevator> elevators;
     private boolean scanDirection = true; // kierunek ruchu algorytmu SCAN - to be changed
-    private final int ELEVATORS_NUM = 1;
+    private final int ELEVATORS_NUM = 3;
     private final int FLOORS_NUM = 10;
 
     public ElevatorServiceImpl() {
         this.elevators = new ArrayList<>();
+        List<Thread> elevatorsThreads = new ArrayList<>();
         for (int i = 1; i < ELEVATORS_NUM + 1; i++) {
-            this.elevators.add(new Elevator(i, 0, new ArrayList<Integer>(0), true));
+            Elevator elevator = new Elevator(i, 0, new ArrayList<Integer>(0), true);
+            this.elevators.add(elevator);
+            new Thread(elevator).start();
         }
     }
 
@@ -61,7 +64,7 @@ public class ElevatorServiceImpl implements ElevatorService {
                                                                   // floor -> isMovingUp = true -> jedzie do gory, w
                                                                   // odwrotnym rpzypadku isMovingUp = false -> jedzie na
                                                                   // dol
-        this.performSimulationStep();
+        this.performSimulationStep(); // TODO: czy to ma byc tutaj ?!!?!?!??!?! gdzie przeprowadzac ten step?
     }
 
     @Override
@@ -75,86 +78,53 @@ public class ElevatorServiceImpl implements ElevatorService {
 
     @Override
     public void performSimulationStep() {
+
+        // Map<Integer, List<Integer>> elevatorsAvailability = new HashMap<>();
         for (Elevator elevator : elevators) {
-            List<Integer> seekSequence = new ArrayList<Integer>();
-            // int nextFloor;
-
-            System.out.println("current: " + elevator.getCurrentFloor() + "winda: " + elevator.getId());
-            if (elevator.getDestinationFloors().contains(elevator.getCurrentFloor())) {
-                elevator.getDestinationFloors().remove(elevator.getCurrentFloor());
-                System.out.println("winda jest juz na requestowanym pietrze");
-                continue;
-            }
-            if (elevator.getDestinationFloors().isEmpty()) {
-                System.out.println("Prosze daj pietro na ktore chcesz jechac");
-                continue;
-            }
-            System.out.println("podane pietor: " + elevator.getDestinationFloors());
-
-            // jesli jest na drodze to jest ok -> co jesli dostanie inne zgloszenie - "z
-            // drugiej strony"
-            seekSequence = this.SCANAlgoritm(elevator.getDestinationFloors(), elevator.getCurrentFloor(),
-                    elevator.isMovingUp());
-
-            while (!seekSequence.isEmpty()) {
-                int move = elevator.isMovingUp() ? 1 : -1;
-                for (int i = elevator.getCurrentFloor() + move; i != seekSequence.get(0) + move; i += move) {
-                    elevator.setCurrentFloor(i);
-                    System.out.println("Jestem na: " + elevator.getCurrentFloor());
-                    try {
-                        Thread.sleep(1500);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-                seekSequence.remove(0);
-                elevator.setDestinationFloors(seekSequence);
+            if (!elevator.isRunning()) {
+                elevator.run();
             }
         }
-    }
-
-    private List<Integer> SCANAlgoritm(List<Integer> destinationFloorsList, int currentFloor, boolean isMovingUp) {
-        Vector<Integer> up = new Vector<Integer>(), down = new Vector<Integer>();
-        int nowOnFloor = 0;
-        Vector<Integer> seekSequence = new Vector<Integer>();
-
-        for (int i = 0; i < destinationFloorsList.size(); i++) {
-            if (destinationFloorsList.get(i) < currentFloor) {
-                down.add(destinationFloorsList.get(i));
-            }
-            if (destinationFloorsList.get(i) > currentFloor) {
-                up.add(destinationFloorsList.get(i));
-            }
-        }
-
-        Collections.sort(down);
-        Collections.sort(up);
-
-        // napisac wybor czy ma najpierw zaczac poruszac sie w prawo czy w lewo
-        int run = 2;
-        while (run-- > 0) {
-            if (!isMovingUp) {
-                for (int i = down.size() - 1; i >= 0; i--) {
-                    nowOnFloor = down.get(i);
-
-                    // do pozniejszego wypisywania
-                    seekSequence.add(nowOnFloor);
-
-                    currentFloor = nowOnFloor;
-                }
-                isMovingUp = true;
-            } else {
-                for (int i = 0; i < up.size(); i++) {
-                    nowOnFloor = up.get(i);
-
-                    seekSequence.add(nowOnFloor);
-                    currentFloor = nowOnFloor;
-                }
-                isMovingUp = false;
-            }
-        }
-        System.out.println("seekSequence" + seekSequence);
-        return seekSequence;
+        /*
+         * List<Integer> seekSequence = new ArrayList<Integer>();
+         * // int nextFloor;
+         * 
+         * System.out.println("current: " + elevator.getCurrentFloor() + "winda: " +
+         * elevator.getId());
+         * if (elevator.getDestinationFloors().contains(elevator.getCurrentFloor())) {
+         * elevator.getDestinationFloors().remove(elevator.getCurrentFloor());
+         * System.out.println("winda jest juz na requestowanym pietrze");
+         * continue;
+         * }
+         * if (elevator.getDestinationFloors().isEmpty()) {
+         * System.out.println("Prosze daj pietro na ktore chcesz jechac");
+         * continue;
+         * }
+         * System.out.println("podane pietor: " + elevator.getDestinationFloors());
+         * 
+         * // jesli jest na drodze to jest ok -> co jesli dostanie inne zgloszenie - "z
+         * // drugiej strony"
+         * seekSequence = this.SCANAlgoritm(elevator.getDestinationFloors(),
+         * elevator.getCurrentFloor(),
+         * elevator.isMovingUp());
+         * 
+         * while (!seekSequence.isEmpty()) {
+         * int move = elevator.isMovingUp() ? 1 : -1;
+         * for (int i = elevator.getCurrentFloor() + move; i != seekSequence.get(0) +
+         * move; i += move) {
+         * elevator.setCurrentFloor(i);
+         * System.out.println("Jestem na: " + elevator.getCurrentFloor());
+         * try {
+         * Thread.sleep(1000);
+         * } catch (InterruptedException e) {
+         * e.printStackTrace();
+         * }
+         * }
+         * seekSequence.remove(0);
+         * elevator.setDestinationFloors(seekSequence);
+         * }
+         * }
+         */
     }
 
     @Override
